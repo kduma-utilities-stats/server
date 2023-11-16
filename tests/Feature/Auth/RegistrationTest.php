f6a245a3696ffc6fad36a8e5b2a6ca05c84d3fb1
Krystian Duma <git@krystian.duma.sh>
2023-11-16 16:56:01 +0100

    Permit only single user

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -20,5 +21,20 @@ class RegistrationTest extends TestCase
 
         $this->assertAuthenticated();
         $response->assertNoContent();
+    }
+
+    public function test_new_users_cant_register_when_there_is_already_an_account(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post('/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $this->assertGuest();
+        $response->assertForbidden();
     }
 }
